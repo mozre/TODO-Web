32a40085557df947b3d7df99be8528f39226cfb2
mozre <cdxy_zhanjing@163.com>
2017-05-18 20:49:17 +0800

    complete login
    	new file:   .gitignore.txt
    	modified:   .idea/workspace.xml
    	modified:   src/main/java/com/ckt/controller/LoginController.java

--- src/main/java/com/ckt/controller/LoginController.java
+++ src/main/java/com/ckt/controller/LoginController.java
@@ -5,10 +5,12 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,7 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController {
 
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    @RequestMapping(value = "/user/login", method = RequestMethod.GET)
     @ResponseBody
     public String login(HttpServletRequest request, HttpServletResponse response) {
         JSONObject object = new JSONObject();
